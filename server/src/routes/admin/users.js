fd309cc008652c93ed290dc8ec3e7a9dbbbf0a60
abdalrahman hajjo <abedhajjo57@gmail.com>
2026-03-28 00:31:38 +0200

    fix: correct adminMiddleware import path in admin/users.js

--- server/src/routes/admin/users.js
+++ server/src/routes/admin/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { query } = require('../../db');
-const { authMiddleware, adminMiddleware } = require('../../middleware/auth');
+const { authMiddleware } = require('../../middleware/auth');
+const { adminMiddleware } = require('../../middleware/admin');
 const { validate } = require('../../middleware/validation');
 const { adminUserSchema } = require('../../schemas/admin');
 
